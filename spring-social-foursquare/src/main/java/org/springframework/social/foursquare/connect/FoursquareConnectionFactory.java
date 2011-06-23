88a2b74d72ad6259966e8a7aec4625207cfb10ef
Matt W <mdw1980@gmail.com>
2011-06-22 23:23:53 -0400

    Added missing code to connection factory

--- spring-social-foursquare/src/main/java/org/springframework/social/foursquare/connect/FoursquareConnectionFactory.java
+++ spring-social-foursquare/src/main/java/org/springframework/social/foursquare/connect/FoursquareConnectionFactory.java
@@ -1,5 +1,12 @@
 package org.springframework.social.foursquare.connect;
 
-public class FoursquareConnectionFactory {
+import org.springframework.social.connect.support.OAuth2ConnectionFactory;
+import org.springframework.social.foursquare.api.Foursquare;
 
+public class FoursquareConnectionFactory extends OAuth2ConnectionFactory<Foursquare> {
+
+    public FoursquareConnectionFactory(String clientId, String clientSecret) {
+        super("foursquare", new FoursquareServiceProvider(clientId, clientSecret), new FoursquareAdapter());
+    }
+    
 }
