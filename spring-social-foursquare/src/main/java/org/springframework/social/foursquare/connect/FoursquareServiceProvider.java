a0ad79dbd8551d6a663d5913ea4300b6eae8ea37
Matt W <mdw1980@gmail.com>
2011-06-23 12:12:54 -0400

    Removed old files and attempting service provider fix

--- spring-social-foursquare/src/main/java/org/springframework/social/foursquare/connect/FoursquareServiceProvider.java
+++ spring-social-foursquare/src/main/java/org/springframework/social/foursquare/connect/FoursquareServiceProvider.java
@@ -7,14 +7,16 @@ import org.springframework.social.oauth2.AbstractOAuth2ServiceProvider;
 public class FoursquareServiceProvider extends AbstractOAuth2ServiceProvider<Foursquare> {
     
     private final String clientId;
+    private final String clientSecret;
     
     public FoursquareServiceProvider(String clientId, String clientSecret) {
         super(new FoursquareOAuth2Template(clientId, clientSecret));
         this.clientId = clientId;
+        this.clientSecret = clientSecret;
     }
     
     public Foursquare getApi(String accessToken) {
-        return new FoursquareTemplate(clientId, accessToken);
+        return new FoursquareTemplate(clientId, clientSecret, accessToken);
     }
 
 }
